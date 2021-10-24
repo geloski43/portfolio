289bcb806b98f486695665bc20c89c9d22e6dde5
geloski43 <65926526+geloski43@users.noreply.github.com>
2021-10-25 05:27:06 +0800

    add placeholder to projct images

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,19 @@
 import Tween from 'rc-tween-one';
 import React, { useState, useEffect } from 'react';
-import AnimatedSvg from "../components/AnimatedSvg"
-import { Tooltip, message, Typography } from "antd";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AnimatedSvg from '../components/AnimatedSvg';
+import { Tooltip, message, Typography } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import useViewport from '../UseViewPort';
 import MobileAnimatedSvg from '../components/MobileAnimatedSvg';
 
 const { Title } = Typography;
 
 const Home = () => {
-
-  const [tweenData, setTweenData] = useState({ translateX: '100px', duration: 3000 });
-  const [monster, setMonster] = useState("Kapitan Smiley")
+  const [tweenData, setTweenData] = useState({
+    translateX: '100px',
+    duration: 3000,
+  });
+  const [monster, setMonster] = useState('Kapitan Smiley');
   const [monsters, setMonsters] = useState([]);
   const [imageLoading, setImageLoading] = useState(false);
 
@@ -20,49 +22,64 @@ const Home = () => {
 
   const info = () => {
     message.info({
-      className: "message",
+      className: 'message',
       content: ` Greetings! Please click on the animated letters for us to create an army. 
       Sincerely, ${monster}`,
       style: {
         marginTop: '15vh',
-        color: "black",
-        fontWeight: "bolder"
+        color: 'black',
+        fontWeight: 'bolder',
       },
-      duration: 5.5,
-      icon: <FontAwesomeIcon icon={['fas', 'comment-dots']} transform="grow-5" color="#2ede17" />
+      duration: 3.5,
+      icon: (
+        <FontAwesomeIcon
+          icon={['fas', 'comment-dots']}
+          transform="grow-5"
+          color="#2ede17"
+        />
+      ),
     });
   };
 
   const warning = () => {
     message.warning({
-      className: "message",
+      className: 'message',
       content: ` ${monster} reporting for duty.`,
       style: {
         marginTop: '15vh',
-        color: "black",
-        fontWeight: "bolder"
+        color: 'black',
+        fontWeight: 'bolder',
       },
-      duration: 2,
-      icon: <FontAwesomeIcon icon={['fas', 'comment-dots']} transform="grow-5" color="#2ede17" />
+      duration: 1.5,
+      icon: (
+        <FontAwesomeIcon
+          icon={['fas', 'comment-dots']}
+          transform="grow-5"
+          color="#2ede17"
+        />
+      ),
     });
   };
 
   const getRandomString = (length) => {
-    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var randomChars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var result = '';
     for (var i = 0; i < length; i++) {
-      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+      result += randomChars.charAt(
+        Math.floor(Math.random() * randomChars.length)
+      );
     }
     return result;
   };
 
   const maxWarning = () => {
     message.warning({
-      content: "Max monsters reached! Page will reload..",
+      content: 'Max monsters reached! Page will reload..',
       style: {
         marginTop: '15vh',
       },
-      duration: 5
+      duration: 5,
     });
   };
 
@@ -71,13 +88,13 @@ const Home = () => {
       maxWarning();
       setTimeout(() => {
         window.location.reload();
-      }, 4000)
+      }, 4000);
     }
     setImageLoading(true);
-    setMonster(getRandomString(Math.floor((Math.random() * 30) + 1)))
+    setMonster(getRandomString(Math.floor(Math.random() * 30 + 1)));
     setTimeout(() => {
       setImageLoading(false);
-    }, 900)
+    }, 900);
   };
 
   useEffect(() => {
@@ -90,70 +107,109 @@ const Home = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      setTweenData({ opacity: 0.5, marginTop: 90, marginLeft: 0, duration: 1000 })
+      setTweenData({
+        opacity: 0.5,
+        marginTop: 90,
+        marginLeft: 0,
+        duration: 1000,
+      });
     }, 1000);
     setTimeout(() => {
       setTweenData({ opacity: 2 });
-    }, 2000)
+    }, 2000);
   }, []);
 
   useEffect(() => {
-    setMonsters([{ name: monster, url: `https://robohash.org/${monster}?set=set2;size=100x100` }]);
+    setMonsters([
+      {
+        name: monster,
+        url: `https://robohash.org/${monster}?set=set2;size=100x100`,
+      },
+    ]);
     info();
   }, []);
 
   return (
     <>
-      <div style={{ marginRight: 35, marginBottom: 50, display: "flex", flexDirection: "row", justifyContent: "center", flexWrap: "wrap" }}>
+      <div
+        style={{
+          marginRight: 35,
+          marginBottom: 50,
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+        }}
+      >
         <Tween
           animation={tweenData}
-          style={{ height: 400, width: width < breakpoint ? 300 : 1200, textAlign: 'center' }}
+          style={{
+            height: 400,
+            width: width < breakpoint ? 300 : 1200,
+            textAlign: 'center',
+          }}
         >
-          {monsters && monsters.length > 1 &&
+          {monsters && monsters.length > 1 && (
             <div
               style={{ width: 160, marginLeft: width < breakpoint ? 65 : 520 }}
-              className="title">
+              className="title"
+            >
               <Title
                 level={5}
                 style={{
                   fontSize: width < breakpoint ? 8 : 16,
-                  color: "#a94eca",
-                  background: "#1b1a2d",
-                  borderRadius: 4
-                }}>
-                {`${monsters[0].name}${monsters.length > 1 ? " and squad" : ""}`}
-              </Title>
-            </div>}
-
-          {monsters && monsters.map((monster, i) => (
-            <Tooltip
-              color="black"
-              key={i}
-              placement="bottom"
-              title={<div style={{ color: "black" }}>
-                <span
-                  style={{ background: "linear-gradient(to right, rgb(8, 8, 8), rgb(54, 54, 54))", color: "orange", fontSize: 15 }}
-                  className="tool-title">
-                  {monster.name}
-                </span>
-              </div>}
-            >
-              <img
-                style={{
-                  height: width < breakpoint ? 45 : 100,
-                  marginTop: 50,
-                  marginRight: 10,
-                  cursor: "grabbing",
-                  background: "linear-gradient(to right, rgb(97, 112, 101), rgb(26, 116, 1))",
-                  borderRadius: 5
+                  color: '#a94eca',
+                  background: '#1b1a2d',
+                  borderRadius: 4,
                 }}
-                src={monster.url} alt="gelo" />
-            </Tooltip>
-          ))}
+              >
+                {`${monsters[0].name}${
+                  monsters.length > 1 ? ' and squad' : ''
+                }`}
+              </Title>
+            </div>
+          )}
+
+          {monsters &&
+            monsters.map((monster, i) => (
+              <Tooltip
+                color="black"
+                key={i}
+                placement="bottom"
+                title={
+                  <div style={{ color: 'black' }}>
+                    <span
+                      style={{
+                        background:
+                          'linear-gradient(to right, rgb(8, 8, 8), rgb(54, 54, 54))',
+                        color: 'orange',
+                        fontSize: 15,
+                      }}
+                      className="tool-title"
+                    >
+                      {monster.name}
+                    </span>
+                  </div>
+                }
+              >
+                <img
+                  style={{
+                    height: width < breakpoint ? 45 : 100,
+                    marginTop: 50,
+                    marginRight: 10,
+                    cursor: 'grabbing',
+                    background:
+                      'linear-gradient(to right, rgb(97, 112, 101), rgb(26, 116, 1))',
+                    borderRadius: 5,
+                  }}
+                  src={monster.url}
+                  alt="gelo"
+                />
+              </Tooltip>
+            ))}
         </Tween>
 
-
-        {width > breakpoint ?
+        {width > breakpoint ? (
           <AnimatedSvg
             setMonster={setMonster}
             monster={monster}
@@ -163,8 +219,7 @@ const Home = () => {
             setImageLoading={setImageLoading}
             imageLoading={imageLoading}
           />
-          :
-
+        ) : (
           <MobileAnimatedSvg
             setMonster={setMonster}
             monster={monster}
@@ -174,12 +229,11 @@ const Home = () => {
             setImageLoading={setImageLoading}
             imageLoading={imageLoading}
           />
-        }
+        )}
         {/* {alert(width)} */}
       </div>
-
     </>
-  )
-}
+  );
+};
 
 export default Home;
--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -1,72 +1,107 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from 'react';
 import { Card, Tooltip } from 'antd';
-import { projectData } from "../data/projectData"
+import { projectData } from '../data/projectData';
 import Tilt from 'react-tilt';
-import useViewport from "../UseViewPort";
-import Pagination from "../components/Pagination";
+import useViewport from '../UseViewPort';
+import Pagination from '../components/Pagination';
 
 let PageSize = 4;
 
 const Projects = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [imageLoading, setImageLoading] = useState(false);
 
   const { width } = useViewport();
   const breakpoint = 650;
+
+  const showImagePlaceHOlder = () => {
+    setImageLoading(true);
+    setTimeout(() => {
+      setImageLoading(false);
+    }, 1000);
+  };
 
   const currentTableData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
     return projectData.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, projectData]);
+
+  useEffect(() => {
+    showImagePlaceHOlder();
   }, [currentPage]);
 
   return (
-    < >
+    <>
       {currentTableData.map((project, i) => (
         <Tooltip
           color="gray"
           key={i}
           placement="right"
-          title={<div style={{ color: "black" }}>
-            <span
-              style={{ background: "linear-gradient(to right, rgb(8, 8, 8), rgb(54, 54, 54))", color: "yellow" }}
-              className="tool-title">
-              {project.name}
-            </span>
-            <p>{project.description}</p>
-            <a style={{ color: "cyan" }} rel="noopener noreferrer" target="_blank" href={project.url}>{project.url}</a>
-          </div>}
+          title={
+            <div style={{ color: 'black' }}>
+              <span
+                style={{
+                  background:
+                    'linear-gradient(to right, rgb(8, 8, 8), rgb(54, 54, 54))',
+                  color: 'yellow',
+                }}
+                className="tool-title"
+              >
+                {project.name}
+              </span>
+              <p>{project.description}</p>
+              <a
+                style={{ color: 'cyan' }}
+                rel="noopener noreferrer"
+                target="_blank"
+                href={project.url}
+              >
+                {project.url}
+              </a>
+            </div>
+          }
         >
           <Tilt options={{ max: 5 }}>
             <Card
               hoverable
               style={{ width: 300, height: 400, marginBottom: 30 }}
               cover={
-                <img
-                  style={{
-                    height: 400,
-                  }}
-                  src={project.thumb} alt="proj" />
-
-              }>
-            </Card>
+                !imageLoading ? (
+                  <img
+                    style={{
+                      height: 400,
+                    }}
+                    src={project.thumb}
+                    alt="proj"
+                  />
+                ) : (
+                  <div class="ui placeholder">
+                    <div
+                      style={{
+                        height: 400,
+                      }}
+                      class="image"
+                    ></div>
+                  </div>
+                )
+              }
+            ></Card>
           </Tilt>
         </Tooltip>
       ))}
 
-      <div
-        style={{ marginTop: 50 }}
-      >
+      <div style={{ marginTop: 50 }}>
         <Pagination
           className="pagination-bar"
           currentPage={currentPage}
           totalCount={projectData.length}
           pageSize={PageSize}
-          onPageChange={page => setCurrentPage(page)}
+          onPageChange={(page) => setCurrentPage(page)}
         />
       </div>
-
     </>
-  )
+  );
 };
 
 export default Projects;
